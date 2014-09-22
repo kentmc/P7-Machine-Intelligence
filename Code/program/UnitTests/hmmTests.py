fb04dbbdd8e9cca897e9fd5206a1dd27af6b08bb
Kent Caspersen <kcaspe11@student.aau.dk>
2014-09-22 15:20:20 +0200

    almostEqual instead of equal in unittests

--- Code/program/UnitTests/hmmTests.py
+++ Code/program/UnitTests/hmmTests.py
@@ -7,11 +7,11 @@ class TestHMM(unittest.TestCase):
         hmm.emission_matrix = [[0.5, 0.3, 0.2], [0, 1, 0], [0.1, 0.1, 0.8]]
         hmm.initial_matrix = [0.1, 0.2, 0.7]
         hmm.transition_matrix = [[0, 1, 0], [0.85, 0.1, 0.05], [0.3, 0.2, 0.5]]
-        self.assertEqual(hmm.calc_probability([0]), 0.0322)
+        self.assertAlmostEqual(hmm.calc_probability([0]), 0.0322, 7)
 
     def test_hmm_2States_3Sym_1Char(self):
-	hmm = HMM(2, 3, False)
-	hmm.emission_matrix = [[0.1, 0.5, 0.2, 0.2], [0, 0, 0.5, 0.5]]
-	hmm.initial_matrix = [0.2, 0.8]
-	hmm.transition_matrix = [[0.8, 0.2], [0.8, 0.2]]
-	self.assertEqual(hmm.calc_probability([0]), 0.004)
+    	hmm = HMM(2, 3, False)
+    	hmm.emission_matrix = [[0.1, 0.5, 0.2, 0.2], [0, 0, 0.5, 0.5]]
+    	hmm.initial_matrix = [0.2, 0.8]
+    	hmm.transition_matrix = [[0.8, 0.2], [0.8, 0.2]]
+    	self.assertAlmostEqual(hmm.calc_probability([0]), 0.0052, 7)
