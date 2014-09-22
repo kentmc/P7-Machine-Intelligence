8d48a6d454d18c1501d96fa9a910e1779c9e2b26
Theis Christensen <christensen.theis@gmail.com>
2014-09-22 06:03:28 -0700

    unit tested added for 2 state model with 3 symbols

--- Code/program/UnitTests/hmmTests.py
+++ Code/program/UnitTests/hmmTests.py
@@ -2,9 +2,16 @@ import unittest
 from hmm import HMM
 
 class TestHMM(unittest.TestCase):
-    def test_hmm_3_2_1(self):
+    def test_hmm_3States_2Sym_1Char(self):
         hmm = HMM(3, 2, False)
         hmm.emission_matrix = [[0.5, 0.3, 0.2], [0, 1, 0], [0.1, 0.1, 0.8]]
         hmm.initial_matrix = [0.1, 0.2, 0.7]
         hmm.transition_matrix = [[0, 1, 0], [0.85, 0.1, 0.05], [0.3, 0.2, 0.5]]
         self.assertEqual(hmm.calc_probability([0]), 0.0322)
+
+    def test_hmm_2States_3Sym_1Char(self):
+	hmm = HMM(2, 3, False)
+	hmm.emission_matrix = [[0.1, 0.5, 0.2, 0.2], [0, 0, 0.5, 0.5]]
+	hmm.initial_matrix = [0.2, 0.8]
+	hmm.transition_matrix = [[0.8, 0.2], [0.8, 0.2]]
+	self.assertEqual(hmm.calc_probability([0]), 0.004)
