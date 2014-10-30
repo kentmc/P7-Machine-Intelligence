6cb02e078f351cb79a32fedafb8a064c2f133664
Kent Caspersen <kentcasps@msn.com>
2014-10-30 15:20:14 +0100

    improved CLI to choose filepath

--- Code/csharp/ModelLearning/Program.cs
+++ Code/csharp/ModelLearning/Program.cs
@@ -16,13 +16,14 @@ namespace ModelLearning {
             };
 
             while (true) {
+                string output_file = ShowInterfaceSelectOutputFile();
                 List<Learner> selected_learners = ShowInterfaceSelectLearners(learners);
                 if (selected_learners == null)
                     continue;
                 List<int> selected_datasets = ShowInterfaceSelectDatasets(10);
                 if (selected_datasets == null)
                     continue;
-                Benchmarker.Run(selected_learners, selected_datasets, "benchmark_result.txt");
+                Benchmarker.Run(selected_learners, selected_datasets, output_file);
                 Console.WriteLine("Benchmarking has finished with success!");
                 Console.WriteLine("Do another benchmark ? y/n");
                 string response = Console.ReadLine();
@@ -33,15 +34,25 @@ namespace ModelLearning {
             }
         }
 
+        static string ShowInterfaceSelectOutputFile() {
+            Console.WriteLine("\nEnter name for the output file (default is 'benchmark_result.txt')");
+            string response = Console.ReadLine();
+            return response == "" ? "benchmark_result.txt" : response;
+        }
+
         static List<int> ShowInterfaceSelectDatasets(int num_datasets) {
             List<int> selected_datasets = new List<int>();
-            Console.WriteLine("\nChoose datasets [0-" + (num_datasets - 1) + "] (e.g. '02' for 0 and 2):");
+            Console.WriteLine("\nChoose datasets [1-" + (num_datasets) + "] (e.g. '1,5' for 1 and 5):");
             string response = Console.ReadLine();
-            for (int i = 0; i < response.Length; i++) {
-                int dataset_index = Int32.Parse(response[i].ToString());
+            foreach (string s in response.Split(',')) {
+                int dataset_index;
+                if (!Int32.TryParse(s, out dataset_index)) {
+                    Console.WriteLine("Could not parse dataset index: '" + s +"'");
+                    return null;
+                }
                 selected_datasets.Add(dataset_index);
-                if (dataset_index < 0 || dataset_index >= num_datasets) {
-                    Console.WriteLine("Dataset must be within range [0-9], received: " + response);
+                if (dataset_index <= 0 || dataset_index > num_datasets) {
+                    Console.WriteLine("Dataset must be within range [1-"+num_datasets+"], received: " + response);
                     return null;
                 }
             }
@@ -53,10 +64,14 @@ namespace ModelLearning {
             for (int i = 0; i < learners.Count; i++) {
                 Console.WriteLine(i + ": " + learners[i].Name());
             }
-            Console.WriteLine("Choose Learners: (e.g. '02') for 0 and 2");
+            Console.WriteLine("\nChoose Learners: (e.g. '0,2') for 0 and 2");
             string response = Console.ReadLine();
-            for (int i = 0; i < response.Length; i++) {
-                int learner_index = Int32.Parse(response[i].ToString());
+            foreach (string s in response.Split(',')) {
+                int learner_index;
+                if (!Int32.TryParse(s, out learner_index)) {
+                    Console.WriteLine("Could not parse learner index: '" + s +"'");
+                    return null;
+                }
                 if (learner_index < 0 || learner_index >= learners.Count) {
                     Console.WriteLine("Learner index must be within range [0-" + learners.Count + "], received " + learner_index);
                     return null;
