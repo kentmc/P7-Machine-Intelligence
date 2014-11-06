73bc839c2b8cc0e2693979f4dc60cc54cdad9d3d
Roland <anniel12@student.aau.dk>
2014-11-06 10:18:26 +0100

    fixed another error in compile scripts

--- Report/compileMain.sh
+++ Report/compileMain.sh
@@ -1,7 +1,8 @@
 REPORT_MAIN=report
-pdflatex -halt-on-error -draftmode -interaction nonstopmode $REPORT_MAIN.tex 
+pdflatex --halt-on-error -draftmode -interaction nonstopmode $REPORT_MAIN.tex 
 pdflatex --halt-on-error -draftmode -interaction nonstopmode $REPORT_MAIN.tex 
 bibtex $REPORT_MAIN 
 makeglossaries $REPORT_MAIN.glo
+pdflatex --halt-on-error -draftmode -interaction nonstopmode $REPORT_MAIN.tex 
 pdflatex --halt-on-error -interaction nonstopmode $REPORT_MAIN.tex 
 ./cleanUp.sh
