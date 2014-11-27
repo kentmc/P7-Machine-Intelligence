3e2ae4392c39f7066b7d1efa3d4a77b085c7a83c
Kent Caspersen <kentcasps@msn.com>
2014-11-27 15:46:12 +0100

    dfsdf

--- Code/csharp/ModelLearning/Benchmark.cs
+++ Code/csharp/ModelLearning/Benchmark.cs
@@ -224,7 +224,7 @@ namespace ModelLearning
 
                 watch.Reset();
 
-                dataSet.SplitData(2.0 / 3.0);
+                dataSet.SplitData(2.0 / 3.0, i);
 
                 watch.Start();
 
