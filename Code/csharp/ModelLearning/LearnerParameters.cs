10514e55b0913e8b9453ad4b054effed90f9338e
Kent Caspersen <kentcasps@msn.com>
2014-12-08 10:43:32 +0100

    greedyxented changes

--- Code/csharp/ModelLearning/LearnerParameters.cs
+++ Code/csharp/ModelLearning/LearnerParameters.cs
@@ -58,6 +58,9 @@ namespace ModelLearning
                     {
                         Console.WriteLine("Configure Greedy Extend Learner:");
 
+                        Console.Write("Intermediate BW iterations:");
+                        AdditionalParameters.Add("BWiterations", Int32.Parse(Console.ReadLine()));
+
                         Console.Write("Final BW threshold:");
                         AdditionalParameters.Add("finalBWThreshold", Double.Parse(Console.ReadLine()));
 
